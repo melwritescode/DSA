91464df1bb9f6debdc3e4df37c2dd31f072732b5
Melanie <46007892+melwritescode@users.noreply.github.com>
2020-11-12 17:38:28 -0700

    Add improved solution

--- JS/FreeCodeCamp/searchAndReplace.js
+++ JS/FreeCodeCamp/searchAndReplace.js
@@ -9,6 +9,8 @@
 // Note
 // Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
 
+//**************************** Solution 1 ****************************/
+
 const myReplace = (str, target, replacement) => {
 	const whichCase = 
 		target[0] === target[0].toUpperCase() 
@@ -23,3 +25,15 @@ const myReplace = (str, target, replacement) => {
 }
 
 myReplace("Let us go to the store", "store", "mall");
+
+//**************************** Solution 2 ****************************/
+
+const myReplace = (str, target, replacement) => {
+	let caseCorrectedReplacement = replacement.toLowerCase()
+	if(target[0] === target[0].toUpperCase()) {
+		caseCorrectedReplacement = replacement.replace(replacement[0], replacement[0].toUpperCase())
+	}
+	return str.replace(target, caseCorrectedReplacement)
+}
+
+myReplace("I think we should look up there", "up", "Down")
